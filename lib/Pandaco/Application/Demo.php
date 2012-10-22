eac7e194f5a2103ed384ec5d0f34331d052d948a
doc <strapolsky@pandaco.net>
2012-10-23 01:11:51 +0200

    * Added broadcast method on Application abstract

--- lib/Pandaco/Application/Demo.php
+++ lib/Pandaco/Application/Demo.php
@@ -2,13 +2,26 @@
 namespace Pandaco\Application;
 
 use Pandaco\Application;
-
 use \Wrench\Connection;
+use \Wrench\Payload\Payload;
 
 /**
  * @author John Mouloud <john.mouloud@pandaco.net>
  */
 class Demo extends Application
 {
+    public function onConnect(Connection $client)
+    {
+        parent::onConnect($client);
+    }
 
+    public function onDisconnect(Connection $client)
+    {
+        parent::onDisconnect($client);
+    }
+
+    public function onData(Payload $data, Connection $client)
+    {
+
+    }
 }
